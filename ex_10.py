f27c6a8a31bf2851de3cb496f92608ea953eb7c8
MPADILLA-25 <marissapadilla.hrm@gmail.com>
2025-10-23 20:38:02 -0700

    Add files via upload

---
+++ ex_10.py
@@ -0,0 +1,35 @@
+fname = input('Enter file: ')
+if len(fname) < 1 : fname = 'clown.txt'
+
+fhand = open(fname)
+
+many = dict()
+
+for line in fhand :
+    line = line.rstrip()
+    wds = line.split()
+
+    for w in wds:
+        many[w] = many.get(w, 0) + 1
+
+# Find the top 5 word by frequency
+
+tmp = dict()
+newlist = list()
+for k,v in many.items() :
+    tup = (v,k)
+    newlist.append(tup)
+
+cool = sorted(newlist, reverse=True)
+
+for v,k in cool[:5] :
+    print(k,v)
+
+
+# largest = None
+# bigword = None
+# for cle, valeur in many.items() :
+#     if largest is None or valeur > largest :
+#         largest = valeur
+#         bigword = cle
+# print(bigword, largest)
